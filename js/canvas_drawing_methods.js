9b5d3b597eabec3c4dbc2dc5f6f87e855cf2af68
Armand_laptop <517226@edu.rocmn.nl>
2018-07-29 16:56:39 +0200

    CirclePieces now have fully drawn borders

--- js/canvas_drawing_methods.js
+++ js/canvas_drawing_methods.js
@@ -19,74 +19,40 @@ function DrawClockLikeWords(array, ctx, radius, startAngle, fontSize) {
 }
 
 // function drawArcs(array) {
-function drawPiePiece(x, y, radius, startAngle, endAngle, color) {
+function DrawPiePiece(x, y, radius, startAngle, endAngle, color) {
     ctx.beginPath();
-    ctx.moveTo(x, y);
     ctx.arc(x, y, radius, startAngle * Math.PI, endAngle * Math.PI, false);
     ctx.fillStyle = color;
     ctx.fill();
 }
 
 // function drawArcs(array) {
-function drawCirclePiece(x, y, radius, thickness, startAngle, endAngle, color) {
+function DrawCirclePiece(x, y, radius, thickness, startAngle, endAngle) {
+    let startXY = CalcXYBasedOnSinRule(startAngle, radius - thickness, x, y);
+    let firstLine = CalcXYBasedOnSinRule(startAngle, radius, x, y);
+
     ctx.beginPath();
-    ctx.moveTo(x, y);
+
+    ctx.moveTo(startXY.x, startXY.y);
+    ctx.lineTo(firstLine.x, firstLine.y);
+
     ctx.arc(x, y, radius,               startAngle * Math.PI,   endAngle * Math.PI, false);
     ctx.arc(x, y, radius - thickness,   endAngle * Math.PI,     startAngle * Math.PI, true);
-
-    ctx.fillStyle = color;
-    ctx.fill();
 }
 
-function drawCircle(array) {
+function DrawCircle(array) {
     for (let i = 0; i < array.length; i++) {
-        drawCirclePiece(array[i]["x"], array[i]["y"], array[i].radius, array[i].thickness, array[i].startAngle, array[i].endAngle, array[i]["color"]);
+        DrawCirclePiece(array[i]["x"], array[i]["y"], array[i].radius, array[i].thickness, array[i].startAngle, array[i].endAngle);
         ctx.strokeStyle = "000";
         ctx.lineWidth = 0.4;
+        ctx.fillStyle = array[i]["color"];
+        ctx.fill();
         ctx.stroke();
     }
 }
 
 function DrawPie(array) {
     for (let i = 0; i < array.length; i++) {
-        drawPiePiece(array[i]["x"], array[i]["y"], array[i].radius, array[i].startAngle, array[i].endAngle, array[i]["color"]);
+        DrawPiePiece(array[i]["x"], array[i]["y"], array[i].radius, array[i].startAngle, array[i].endAngle, array[i]["color"]);
     }
-}
-
-function getOffsetY( elem ) {
-    if ( document.getElementById ) {
-        elem = document.getElementById ( elem );
-    }
-    else if ( document.all ) {
-        elem = document.all[elem];
-    }
-
-    var yPos = elem.offsetTop;
-    var tempEl = elem.offsetParent;
-
-    while ( tempEl != null ) {
-        yPos += tempEl.offsetTop;
-        tempEl = tempEl.offsetParent;
-    }
-    return yPos;
-}
-
-// needed for the offset function
-function getOffsetX( elem ) {
-
-    if ( document.getElementById ) {
-        elem = document.getElementById ( elem );
-    }
-    else if ( document.all ) {
-        elem = document.all[elem];
-    }
-
-    let yPos = elem.offsetLeft;
-    let tempEl = elem.offsetParent;
-
-    while ( tempEl != null ) {
-        yPos += tempEl.offsetLeft;
-        tempEl = tempEl.offsetParent;
-    }
-    return yPos;
 }
