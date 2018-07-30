8474e817a70ff42ce1f99475c69a09649ca97cf8
Armand_laptop <517226@edu.rocmn.nl>
2018-07-30 03:18:47 +0200

    removed animation code from piePieces.js

--- js/piePieces.js
+++ js/piePieces.js
@@ -39,37 +39,3 @@ function ReGenerateCanvas() {
 
 ReGenerateCanvas()
 window.addEventListener('resize', RenderCanvas);
-
-
-function RotateWheel() {
-
-    this.run = function() {
-        for (var i = 0; i < circleParts.length; i++) {
-            circleParts[i].startAngle -= this.angle;
-            circleParts[i].endAngle -= this.angle;
-        }
-        words[0].startAngle -= this.angle;
-        RenderCanvas();
-    }
-
-    this.clockWise = function() {
-        this.angle = -0.0015;
-        this.run()
-    }
-
-    this.counterClockWise = function() {
-        this.angle = 0.0015;
-        this.run()
-    }
-}
-
-// const rotate = new RotateWheel;
-// setInterval(function () {
-//     // console.log("")
-//     // for (let i = 0; i < circleParts.length; i++) {
-//     //     // circleParts[i].radius += 0.20;
-//     //     // circleParts[i].thickness -= 2;
-//     // }
-//
-//     rotate.clockWise();
-// }, 66);
