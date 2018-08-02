6f23a040ba2c6feb15b9b76b37512fde46886e88
Armand_laptop <517226@edu.rocmn.nl>
2018-08-02 19:53:41 +0200

    refactored draw_rectangle to es6 oo

--- js/canvas_drawing_methods/draw_rectangle.js
+++ js/canvas_drawing_methods/draw_rectangle.js
@@ -1,16 +1,14 @@
-function DrawRectangle(startX, startY, height, width, color) {
-    this.x = startX;
-    this.y = startY;
-    this.width = width;
-    this.height = height;
-    this.color = color;
+class DrawRectangle() {
+    constructor(startX, startY, height, width, color) {
+        this.x = startX;
+        this.y = startY;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
 
     this.render = function() {
         ctx.fillStyle = this.color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
-
-    ctx.rect(this.x, this.y, this.width, this.height);
-    ctx.fillStyle = this.color;
-    ctx.fill();
 }
